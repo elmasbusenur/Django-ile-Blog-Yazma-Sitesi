622233c412e3cf0469ec8540396db1d71485bb97
elmasbusenur <elmasbusenur@gmail.com>
2020-03-31 21:25:41 +0300

    Proje İlk Upload

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    text="Django Kurulum: python -m pip install -e django/ <br> Proje Oluşturma: django-admin startproject mysite <br> App Ekleme: python manage.py startapp polls"
+    context = {'text': text}
+    return render(request, 'index.html', context)
