ca6f77ea2acce88e1dfc89bfbaa41b887e7ed2e3
elmasbusenur <elmasbusenur@gmail.com>
2020-04-05 15:54:22 +0300

    4.video tamamlandı.Model kategori ve blog ilişkisi

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,16 @@
-from blog.models import Category
+from blog.models import Category, Blog
 from django.contrib import admin
 
-#class tanımlıyoruz. bu kategori ile yapacağım ayarları
+
+# class tanımlıyoruz. bu kategori ile yapacağım ayarları
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status'] #hangi alanlar görülsün istiyorum
-    list_filter = ["status"] # statuye göre filtreliyorum
+    list_display = ['title', 'status']  # hangi alanlar görülsün istiyorum
+    list_filter = ['status']  # statuye göre filtreliyorum
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'title', 'amount', 'status']  # hangi alanlar görülsün istiyorum
+    list_filter = ['status','category']  # statuye göre filtreliyorum
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
+
 from django.db import models
 
 
 # Create your models here.
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -13,7 +16,28 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE) #foreignkey self kendi id sine refere edicek
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model): #kategori ile ilişki kuruyoruz
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #foreignkey kategori: kategoriye refere ettik
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
