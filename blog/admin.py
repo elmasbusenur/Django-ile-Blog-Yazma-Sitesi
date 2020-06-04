21f10a84b0cc850191c4449a83f45c974f169ff3
elmasbusenur <elmasbusenur@gmail.com>
2020-06-04 23:32:51 +0300

    8. video tamamlandı. Adminde yuklenen resimler kategori kısmında gözüküyor

--- blog/admin.py
+++ blog/admin.py
@@ -9,22 +9,24 @@ class BlogImageInline(admin.TabularInline):
 
 # class tanımlıyoruz. bu kategori ile yapacağım ayarları
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']  # hangi alanlar görülsün istiyorum
+    list_display = ['title', 'status', 'image_tag']  # hangi alanlar görülsün istiyorum
+    readonly_fields = ('image_tag',)
     list_filter = ['status']  # statuye göre filtreliyorum
 
-
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'title', 'amount', 'status']  # hangi alanlar görülsün istiyorum
-    list_filter = ['status','category']  # statuye göre filtreliyorum
+    list_display = ['title', 'image_tag', 'status'] # hangi alanlar görülsün istiyorum
+    readonly_fields =('image_tag',)
+    list_filter = ['status', 'category'] # statuye göre filtreliyorum
     inlines = [BlogImageInline] #blogimagesinline ürün ekleme sırasında istiyorsam burayada bağlantısını kurmalıyım
-    #blogla ilgili olduğu için sadece onunla alakalı şeyleri ekler
-#imageye class ekliyorum
+    '''blogla ilgili olduğu için sadece onunla alakalı şeyleri ekler
+imageye class ekliyorum'''
+
 class ImagesAdmin(admin.ModelAdmin):
-    list_display = ['title', 'blog', 'image']  # hangi alanlar görülsün istiyorum
+    list_display = ['title', 'blog']  # hangi alanlar görülsün istiyorum
+    readonly_fields = ('image_tag',)
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Blog, BlogAdmin)
 # Register your models here.
-
 #adminde images gözümesi için
-admin.site.register(Images,ImagesAdmin) #sitede de gzükmesi için ımagesadminle bağlntı kuruyorum
+admin.site.register(Images, ImagesAdmin) #sitede de gzükmesi için ımagesadminle bağlntı kuruyorum
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
-
 from django.db import models
 
-
 # Create your models here.
+from django.utils.safestring import mark_safe
 
 
 class Category(models.Model):
@@ -16,7 +15,8 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE) #foreignkey self kendi id sine refere edicek
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children',
+                               on_delete=models.CASCADE)  # foreignkey self kendi id sine refere edicek
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -24,12 +24,19 @@ class Category(models.Model):
         return self.title
 
 
-class Blog(models.Model): #kategori ile ilişki kuruyoruz
-    STATUS = (
+    def image_tag(self):   #image diye html kodu yazıyoruz adı img tag.class image in altında ki image burada ki image url kısmı search kısmına yazılıyor
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
+
+
+class Blog(models.Model):  # kategori ile ilişki kuruyoruz
+    STATUS = [
         ('True', 'Evet'),
         ('False', 'Hayır'),
-    )
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) #foreignkey kategori: kategoriye refere ettik
+    ]
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # foreignkey kategori: kategoriye refere ettik
     title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
@@ -43,20 +50,31 @@ class Blog(models.Model): #kategori ile ilişki kuruyoruz
 
     def __str__(self):
         return self.title
-#ilk olarak ilişki kuracağım alanı yzıyorum blog tablosu..cascade= on delete durumunda (drapdown)ilişkili olduğu product silinirse buda silinmeli
-#bağlı olduğu şey silinirse alt alan havada kalır oda silinmeli
-class Images(models.Model):
-    blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
-    title = models.CharField(max_length=50,blank=True)#blankTrue dersek etiketi boş geçmemize izin verir
-    image = models.ImageField(blank=True, upload_to='images/')
-    #bu değişikliği yaptıktan sonra veritabanında migrate etmek gerekir
-#python manage.py makemigrations  şu dosyayı oluşturdum
-#python manage.py migrate ile de migrate ediyorum veri tabanına işliyor
-#veritabanında title image bolog_id oluştu
-#django frameworkte id ler otomatik oluşur
 
-    #bu model ne döndersin..veritabanında şimdi ürünlerin ismi gözüküyor
+    def image_tag(self):   #image diye html kodu yazıyoruz adı img tag.class image in altında ki image burada ki image url kısmı search kısmına yazılıyor
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
+# ilk olarak ilişki kuracağım alanı yzıyorum blog tablosu..cascade= on delete durumunda (drapdown)ilişkili olduğu
+# product silinirse buda silinmeli bağlı olduğu şey silinirse alt alan havada kalır oda silinmeli
+
+class Images(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)  # blankTrue dersek etiketi boş geçmemize izin verir
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    # bu değişikliği yaptıktan sonra veritabanında migrate etmek gerekir
+    # python manage.py makemigrations  şu dosyayı oluşturdum
+    # python manage.py migrate ile de migrate ediyorum veri tabanına işliyor
+    # veritabanında title image bolog_id oluştu
+    # django frameworkte id ler otomatik oluşur
+
+    # bu model ne döndersin..veritabanında şimdi ürünlerin ismi gözüküyor
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
 
+    image_tag.short_description = 'Image'
 
