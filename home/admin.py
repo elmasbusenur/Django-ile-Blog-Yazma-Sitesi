74b5a2c285179804eb80ed7b4cb16dc667236985
elmasbusenur <elmasbusenur@gmail.com>
2020-06-06 02:24:45 +0300

    13.video tamamlandı.İletişim kısmı halledildi

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 #home/models deki clasın migrations dan sonra çalışabilmesi için register yapmalıyız.
-from home.models import Setting
+from home.models import Setting, ContactFormMessage
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject','message','note','status']
+    list_filter = ['status']
+admin.site.register(ContactFormMessage, ContactFormMessageAdmin)
 admin.site.register(Setting)
--- home/views.py
+++ home/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
+
+
 def index(request):
     setting = Setting.objects.get(pk=1) #primary key 1 i çağırıyor
     context = {'setting': setting}
@@ -20,6 +22,23 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+
+
+    if request.method == 'POST': #form post edildi mi. edilmezse setting e iner.
+        form = ContactFormu(request.POST) #CONTACT FORMUNU POSTLA EŞLEŞTİR
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız başarıyla gönderilmiştir. Teşekkür Ederiz")
+            return HttpResponseRedirect('/iletisim') #formu iletişim sayfasına gönderiyoruz
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
+
