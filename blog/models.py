e1784fd26e7e8e3126d66eda192fa0c04a3461c9
elmasbusenur <elmasbusenur@gmail.com>
2020-06-05 15:51:09 +0300

    11. video tamamlandı. Ckeditor install edildi. blogs->Zonguldak, Bolu image eklendi. settings.py de images yolu güncellendi.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.utils.safestring import mark_safe
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Category(models.Model):
@@ -32,10 +33,10 @@ class Category(models.Model):
 
 
 class Blog(models.Model):  # kategori ile ilişki kuruyoruz
-    STATUS = [
+    STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
-    ]
+    )
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  # foreignkey kategori: kategoriye refere ettik
     title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
@@ -43,7 +44,7 @@ class Blog(models.Model):  # kategori ile ilişki kuruyoruz
     image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
     amount = models.IntegerField()
-    detail = models.TextField()
+    detail = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
